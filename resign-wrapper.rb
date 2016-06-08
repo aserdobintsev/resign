562e9624c38d19c2015a18747740a61a519b10f3
Alexander <aserdobintsev@users.noreply.github.com>
2016-06-08 11:39:53 +0500

    Update resign-wrapper.rb

--- resign-wrapper.rb
+++ resign-wrapper.rb
@@ -1,32 +1,12 @@
-#--################################################################
-# Copyright 2014, Alexander Serdobintsev <aserdobintsev@gmail.com>
-#                                                            
-# Distributed under the MIT license. 
-#Permission is hereby granted, free of charge, to any person obtaining a copy
-#of this software and associated documentation files (the "Software"), to deal
-#in the Software without restriction, including without limitation the rights
-#to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-#copies of the Software, and to permit persons to whom the Software is
-#furnished to do so, subject to the following conditions:
-#
-#The above copyright notice and this permission notice shall be included in
-#all copies or substantial portions of the Software.
-#
-#THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-#IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-#FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-#AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-#LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-#OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-#THE SOFTWARE.
-#
-##############################################################
+#!/usr/bin/env ruby
+
+#This script just help not to type name of identity during using ruby resign.rb ...
+
 require 'getoptlong'
 require 'pathname'
-#!/usr/bin/env ruby
+
 #script_path is current script directory
 script_path = File.expand_path(File.dirname(__FILE__))
-RUBY = File.join(RbConfig::CONFIG['bindir'], RbConfig::CONFIG['ruby_install_name'])
 
 #dev_id is the name of the identity passed in
 dev_id = "iPhone Distribution: Your Company Name (XXXXXXXXXX)"
@@ -53,8 +33,4 @@ opts.each do |opt, arg|
 end
 
 throw "file #{prov_profile_path} does not exist" if !File.exists?(prov_profile_path)
-# system("\"#{RUBY} -{script_path}/resign.rb\" --prov_profile_path \"#{prov_profile_path}\" --app_path \"#{app_path}\" --developerid \"#{dev_id}\"")
-output = `#{RUBY} resign.rb --prov_profile_path \"#{prov_profile_path}\" --app_path \"#{app_path}\" --developerid \"#{dev_id}\"`
-# output.split("\n").each do |line|
-  # puts ""
-# end
+system("\"#{script_path}/resign.rb\" --prov_profile_path \"#{prov_profile_path}\" --app_path \"#{app_path}\" --developerid \"#{dev_id}\"")
